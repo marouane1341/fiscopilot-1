3527ac095d1da8c061f0261801d2e6bb956dd0d5
marouane1341 <marouane1341@gmail.com>
2026-02-15 13:25:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+// Service Worker pour cache hors ligne
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("fiscopilot-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
